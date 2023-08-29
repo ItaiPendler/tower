9df4f6f78f1af2bffcc2a64c90ec08666a0d6c81
Itai Pendler <212519425@idf.il>
2023-08-29 22:46:03 +0300

    MORE COMMIT

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Vue from 'vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>HOME! MAP</h1>
+  </main>
+</template>
